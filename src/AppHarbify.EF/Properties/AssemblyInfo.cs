7a9cc61799747eb38af8cb2d7757e4fb16d46151
Chris Sainty <csainty@hotmail.com>
2012-05-05 11:23:31 +1000

    Changing to oldest supported EF version.

--- src/AppHarbify.EF/Properties/AssemblyInfo.cs
+++ src/AppHarbify.EF/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AppHarbify.EF")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Configuration free EF support for AppHarbor")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("AppHarbify")]
 [assembly: AssemblyProduct("AppHarbify.EF")]
